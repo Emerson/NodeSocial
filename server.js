2e57c12f116098fac2b9b0d9c04e54960e02cbcc
Emerson Lackey <emerson.lackey@gmail.com>
2012-02-12 00:55:47 -0500

    adding hello world response

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 http.createServer(function(request, response) {
-
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end('<h1>hello social</h1>');
 }).listen(8000);
 console.log('Server running at http://127.0.0.1:8000/');
