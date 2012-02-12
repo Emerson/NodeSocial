8b6b50cd480bf57fb2abdb4d796c40563d120fed
Emerson Lackey <emerson.lackey@gmail.com>
2012-02-12 18:19:36 -0500

    Very basic, poorly coded template rendering

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,13 +1,39 @@
+var ejs  = require('ejs'),
+    fs   = require('fs'),
+    path = require('path');
+
+
+function renderTemplate(name, response, layout) {
+	// If no layout is defined, then we should just use the default
+	// application.html.ejs
+	if(!layout) {
+		layout = 'application.html.ejs';
+	}
+	templateString = false;
+	// Get the template data first
+	fs.readFile(path.join(process.cwd(), 'views', 'pages', name), 'utf-8', function(err, data) {
+		if(!err) {
+			templateString = data;
+		}
+	});
+	fs.readFile(path.join(process.cwd(), 'views', 'layout', layout), 'utf-8', function(err, data) {
+		if(!err) {
+			layoutString = data;
+			response.end(ejs.render(layoutString, {body: templateString}));
+		}else{
+			return '<h1>Error Rendering Template: ' + name + '</h1>';
+		}
+	});
+}
+
 module.exports = {
 	
 	home: function(request, response, params) {
-		console.log('pages/home');
-		response.end('<h1>homepage</h1>');
+		renderTemplate('home.html.ejs', response);
 	},
 
 	index: function(request, response, params) {
-		console.log('pages/index');
-		response.end('<h1>index</h1>');
+		renderTemplate('index.html.ejs', response);
 	}
 
 }
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-
-
-
 // Server Request and Response Cycle
 var http = require('http');
 http.createServer(function(request, response) {
 	var router = require('./lib/router');
 	router.parseRequest(request, response);
-
 }).listen(8000);
 console.log('Server running at http://127.0.0.1:8000/');
