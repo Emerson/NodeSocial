fd7ef50730e1d9742128f932fa53911c693956c9
Emerson Lackey <emerson.lackey@gmail.com>
2012-02-12 20:59:31 -0500

    adding template class

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,39 +1,15 @@
-var ejs  = require('ejs'),
-    fs   = require('fs'),
-    path = require('path');
+var path 	 = require('path'),
+    template = require(path.join(process.cwd(), 'lib', 'template'));
 
-
-function renderTemplate(name, response, layout) {
-	// If no layout is defined, then we should just use the default
-	// application.html.ejs
-	if(!layout) {
-		layout = 'application.html.ejs';
-	}
-	templateString = false;
-	// Get the template data first
-	fs.readFile(path.join(process.cwd(), 'views', 'pages', name), 'utf-8', function(err, data) {
-		if(!err) {
-			templateString = data;
-		}
-	});
-	fs.readFile(path.join(process.cwd(), 'views', 'layout', layout), 'utf-8', function(err, data) {
-		if(!err) {
-			layoutString = data;
-			response.end(ejs.render(layoutString, {body: templateString}));
-		}else{
-			return '<h1>Error Rendering Template: ' + name + '</h1>';
-		}
-	});
-}
 
 module.exports = {
 	
 	home: function(request, response, params) {
-		renderTemplate('home.html.ejs', response);
+		template.render('pages/home.html.ejs', response);
 	},
 
 	index: function(request, response, params) {
-		renderTemplate('index.html.ejs', response);
+		template.render('pages/index.html.ejs', response);
 	}
 
 }
